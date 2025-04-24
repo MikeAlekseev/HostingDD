0bdb256a8782b35807db2784d077ef91ed3ce2de
johhnynouz <johhnynouz@yandex.ru>
2025-04-24 18:48:32 +0300

    Добавлена первоначальная разметка

--- src-client/page/MainPage/MainPage.tsx
+++ src-client/page/MainPage/MainPage.tsx
@@ -1,3 +1,20 @@
+import './MainPage.css'
+import {Simulate} from "react-dom/test-utils";
+import dragOver = Simulate.dragOver;
+
 export function MainPage() {
-    return <h1>MainPage</h1>
+    return (
+        <div className="container">
+            <h1>MainPage</h1>
+            <div className="header">
+
+            </div>
+            <div className="main">
+                <div className="main_left"></div>
+                <div className="main_right"></div>
+            </div>
+
+        </div>
+
+    )
 }

49e77c112ad67b8eac63f3fd5213a74f6fce6a5f
johhnynouz <johhnynouz@yandex.ru>
2025-04-27 20:26:53 +0300

    Добавлена разметка основной страницы

--- src-client/page/MainPage/MainPage.tsx
+++ src-client/page/MainPage/MainPage.tsx
@@ -1,17 +1,26 @@
-import './MainPage.css'
+import '../../../MainPage.css'
 import {Simulate} from "react-dom/test-utils";
 import dragOver = Simulate.dragOver;
 
 export function MainPage() {
     return (
-        <div className="container">
+        <div className="top div-bottom-borde">
             <h1>MainPage</h1>
             <div className="header">
-
+                <div>
+                    ТУТ КАКАЯ ТА НАДПИСЬ
+                </div>
+                <div className="login">
+                    Login / Registration
+                </div>
             </div>
-            <div className="main">
+            <div className="main r">
                 <div className="main_left"></div>
-                <div className="main_right"></div>
+                <div className="main_right">
+                    <div className="main_button">
+                        СЮДА ТАЩИТЬ ФАЙЛЫ
+                    </div>
+                </div>
             </div>
 
         </div>
